cce5499a94f25a2c4117f9eab43be55e2a328f81
thomaspeterkueper <thomas.peter.kueper@gmail.com>
2026-07-07 22:25:07 +0200

    Add UI design system barrel export

---
+++ components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Badge } from './Badge';
+export { Button } from './Button';
+export { Card } from './Card';
+export { Container } from './Container';
+export { PageHeader } from './PageHeader';
+export { Section } from './Section';
+export { Stat } from './Stat';
+export { Tabs } from './Tabs';
